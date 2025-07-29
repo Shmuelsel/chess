ea267680ae8cc238aa1bad73a71c3ee01836ca33
Shmuel Selzer <shmuelse@std.hac.ac.il>
2025-07-29 15:22:37 +0300

    קיצור פונקציית לחיצה

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -13,22 +13,22 @@ const GameComponent = ({ onBack }) => {
 
     const handleSquareSelection = (row, col) => {
         if (selectedPiece) {
-            if (game.getBoard().getSquare(row, col).isOccupied() && game.getBoard().getSquare(row, col).getPiece().getColor() === selectedPiece.getColor()) {
-                console.log(`Selecting piece at Row ${row}, Col ${col}`);
-                const square = game.getBoard().getSquare(row, col);
-                const piece = square.getPiece();
-                const leagalMoves = piece.getLegalMoves(row, col, game.getBoard());
-                console.log(`Legal moves for selected piece: ${leagalMoves}`);
+            // if (game.getBoard().getSquare(row, col).isOccupied() && game.getBoard().getSquare(row, col).getPiece().getColor() === selectedPiece.getColor()) {
+            //     console.log(`Selecting piece at Row ${row}, Col ${col}`);
+            //     const square = game.getBoard().getSquare(row, col);
+            //     const piece = square.getPiece();
+            //     const leagalMoves = piece.getLegalMoves(row, col, game.getBoard());
+            //     console.log(`Legal moves for selected piece: ${leagalMoves}`);
 
-                if (leagalMoves.length <= 0) {
-                    console.error("No valid moves for the selected piece.");
-                    return;
-                }
-                setValidMoves(piece.getLegalMoves(row, col, game.getBoard()))
-                setSelectedSquare({ row, col });
-                setSelectedPiece(piece);
-                console.log(`Selected piece: ${piece.getType()} at Row ${row}, Col ${col}`);
-            }
+            //     if (leagalMoves.length <= 0) {
+            //         console.error("No valid moves for the selected piece.");
+            //         return;
+            //     }
+            //     setValidMoves(piece.getLegalMoves(row, col, game.getBoard()))
+            //     setSelectedSquare({ row, col });
+            //     setSelectedPiece(piece);
+            //     console.log(`Selected piece: ${piece.getType()} at Row ${row}, Col ${col}`);
+            // }
             const isValid = validMoves.some(move => move[0] === row && move[1] === col);
             validMoves.forEach(move => {
             });
@@ -45,7 +45,7 @@ const GameComponent = ({ onBack }) => {
 
             }
 
-            return;
+            // return;
         }
         if (game.getBoard().getSquare(row, col).isOccupied() && game.getBoard().getSquare(row, col).getPiece().getColor() === game.getCurrentTurn()) {
             console.log(`Selecting piece at Row ${row}, Col ${col}`);
